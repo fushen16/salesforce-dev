1e56eecd5b4237f96f59ca1af6aad62d4719c81d
fushen16 <fshen@SEA-L-00026976.olympus.F5Net.com>
2018-12-20 09:59:33 -0800

    added this one for the trailhead session, "connect component with events"

--- developer/src/aura/campingList/campingListHelper.js
+++ developer/src/aura/campingList/campingListHelper.js
@@ -1,39 +1,20 @@
 ({
-	createItem : function(component, event) {
+    handleAddItem: function(component, event){
         
-        var validItem = component.find('CampingItemform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);   
-        
-        if(validItem) {
+        var newItem = event.getParam("item");
+        var action = component.get("c.saveItem");
+        action.setParams({
+            "campingItem": newItem
+        });
+        action.setCallback(this, function(response){
             
-            var newItem = component.get("v.newItem");
-            var action = component.get("c.saveItem");
-            action.setParams({
-                "campingItem": newItem
-            });
-            
-            action.setCallback(this, function(response){
-                var state = response.getState();
-                if (state === "SUCCESS") {
-                	var items = component.get("v.items");
-                	items.push(response.getReturnValue());
-                	component.set("v.items", items);
-                	
-                	//reset the new item form
-                    component.set("v.newItem",
-                            {'sobjectType' : 'Camping_Item__c',
-                             'Name' : '',
-                             'Quantity__c' : 0,
-                             'Price__c' : 0,
-                             'Packed__c' : false});  
-                }
-            });
-            
-            $A.enqueueAction(action);        
-        }
-		
-	}
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(response.getReturnValue());
+                component.set("v.items", items);
+            }
+        });
+        $A.enqueueAction(action);   
+    }
 })
